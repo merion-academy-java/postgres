8fb7b0a78086c202d9348968299e5197580274f8
pocteg <pocteg@gmail.com>
2023-07-22 16:50:17 +0300

    Документация для API Swagger

--- src/main/java/com/example/postgres/user/controller/UserApiController.java
+++ src/main/java/com/example/postgres/user/controller/UserApiController.java
@@ -1,11 +1,18 @@
 package com.example.postgres.user.controller;
 
 import com.example.postgres.user.dto.request.CreateUserRequest;
+import com.example.postgres.user.dto.request.EditUserRequest;
 import com.example.postgres.user.dto.response.UserResponse;
 import com.example.postgres.user.entity.UserEntity;
 import com.example.postgres.user.exceptions.UserNotFoundException;
 import com.example.postgres.user.repository.UserRepository;
 import com.example.postgres.user.routes.UserRoutes;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -23,6 +30,7 @@ public class UserApiController {
 
     private final UserRepository userRepository;
 
+    @Operation(summary = "Создание пользователя", description = "Создаем пользователя по имени и фамилии")
     @PostMapping(UserRoutes.CREATE)
     public UserResponse create(@RequestBody CreateUserRequest request) {
         UserEntity user = UserEntity.builder()
@@ -34,8 +42,17 @@ public class UserApiController {
         return UserResponse.of(user);
     }
 
+    @Operation(summary = "Редактирование пользоваеля пользователя", description = "Создаем пользователя по имени и фамилии")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешно отредактированный пользователь",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Не корректный запрос",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Пользователь с таким id не найден",
+                    content = @Content) })
     @PutMapping(UserRoutes.BY_ID)
-    public UserResponse edit(@PathVariable Long id, @RequestBody CreateUserRequest request) {
+    public UserResponse edit(@PathVariable Long id, @RequestBody EditUserRequest request) {
         UserEntity user = userRepository
                 .findById(id)
                 .orElseThrow(UserNotFoundException::new);
